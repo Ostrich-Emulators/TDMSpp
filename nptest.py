bced47e148f187ba550f8909671bc9724425e91e
ryan bobko <ryan@ostrich-emulators.com>
2019-09-18 11:32:18 -0400

    adding release tag to install dir

--- nptest.py
+++
@@ -1,8 +0,0 @@
-from nptdms import TdmsFile
-
-tdms_file = TdmsFile("../FormatConverter/test.medi")
-channel = tdms_file.object('Intellivue', 'CmpndECG(I)')
-data = channel.data
-
-for val in data:
-  print("{0:5.4f}".format(val))
